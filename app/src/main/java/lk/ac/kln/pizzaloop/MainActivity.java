a870fa2f08528d6b4b9c6d257f1a004e0eb38281
RanathungaRAGMT <malithathejanranathunga@gmail.com>
2019-06-14 21:33:27 +0530

    first Upload

---
+++ app/src/main/java/lk/ac/kln/pizzaloop/MainActivity.java
@@ -0,0 +1,14 @@
+package lk.ac.kln.pizzaloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
